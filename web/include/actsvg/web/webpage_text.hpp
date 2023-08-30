a88ed246e04971c9e6cc618d2f15112308995fb8
Frederik <frederik.barba@hotmail.com>
2023-08-30 17:20:20 +0200

    Regenerated webpage content header file

--- web/include/actsvg/web/webpage_text.hpp
+++ web/include/actsvg/web/webpage_text.hpp
@@ -26,8 +26,10 @@ const std::string index_text = R"(<!DOCTYPE html>
             <!-- The checkboxes will be added here -->
         </div>
 
-        <div id="result" class="result">
-            <!-- The resulting SVG will be added here -->
+        <div id="result-div" class="result-div" onmouseover="document.body.style.overflow='hidden';"  onmouseout="document.body.style.overflow='auto';">
+            <svg id="result-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-300 -300 600 600">
+            
+            </svg>
         </div>
     </div>
 
@@ -37,79 +39,18 @@ const std::string index_text = R"(<!DOCTYPE html>
 
 )";
 
-const std::string script_text = R"(// Initialize the page by loading a form with a button for each SVG available.
-// This is done by reading the paths in the config.json file.
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('./config.json').then(response => response.json()).then(json =>{
-        load_form(json);
-    });
-});
+const std::string script_text = R"(let SVGResult = document.getElementById('result-svg');
+let formContainer = document.getElementById('formContainer');
+let SVGContainer = document.getElementById("result-div");
 
-// Loads a form containing a button for each SVG available.
-function load_form(group)
-{
-    let form = document.createElement("form");
-    form.id = "checkboxForm";
-
-    group.forEach(item =>{
-
-        itemDiv = document.createElement("div");
-        itemDiv.classList.add("form-item");
-
-        let label = document.createElement("label");
-        itemDiv.appendChild(label);
-
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.name = "checkbox";
-        checkbox.value = "./svgs/" + item;
-        label.append(checkbox);
-
-        let span = document.createElement("span");
-        span.textContent = get_name(item);
-        label.appendChild(span);
-
-        form.appendChild(itemDiv);
-
-    });
-
-    form.append(document.createElement("br"));
-
-    // Create a button to apply the changes
-    let apply_button = document.createElement("input");
-    apply_button.type = "button";
-    apply_button.value = "Apply Selection";
-    apply_button.onclick = getSelectedValues;
-    apply_button.classList.add("apply-button");
-
-    // Append the apply button to the form
-    form.append(apply_button);
-
-    // Append the form to the formContainer div
-    const formContainer = document.getElementById("formContainer");
-    formContainer.appendChild(form);
-}
-
-// For removing the file extension.
-function get_name(path){
-    return path.replace(".svg", "");
-}
-
-// Updates the displayed SVG by combining the selected SVGs.
-async function getSelectedValues() {
-    var checkboxes = document.getElementsByName("checkbox");
-    var selectedValues = [];
-
-    for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            selectedValues.push(checkboxes[i].value);
-        }
+// Given a collection of paths, returns a collection of containing the respective file text.
+async function readFiles(paths){
+    let result = []
+    for (const path of paths) {
+        let data = await (await fetch(path)).text();
+        result.push(data);
     }
-    
-    selectedValues = selectedValues.reverse();
-    let svg = await combineSVGS(selectedValues);
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = svg;
+    return result;
 }
 
 // Removes the <svg> and </svg> tag to obtain its content.
@@ -121,39 +62,146 @@ function removeSVGTag(data){
     return data;
 }
 
-// Given a collection of paths, returns a collection of containing the respective file text.
-async function readFiles(paths){
-    let result = []
-    for (const path of paths) {
-        try{
-            let data = await (await fetch(path)).text();
-            result.push(data);
-        }
-        catch(err) 
-        {
-            console.error(err);
+async function SVGContentMerge(paths){
+    let contents = await readFiles(paths);
+    return contents.map(removeSVGTag).join('\n');
+}
+
+// Loads a form containing a button for each SVG available.
+function load_form(group)
+{
+    let form = document.createElement('form');
+    form.id = 'checkboxForm';
+
+    group.forEach(item =>{
+
+        itemDiv = document.createElement('div');
+        itemDiv.classList.add('form-item');
+
+        let label = document.createElement("label");
+        itemDiv.appendChild(label);
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'checkbox';
+        checkbox.value = './svgs/' + item;
+        label.append(checkbox);
+
+        let span = document.createElement('span');
+        span.textContent = item.replace('.svg', '');
+        label.appendChild(span);
+
+        form.appendChild(itemDiv);
+
+    });
+
+    form.append(document.createElement('br'));
+
+    // Create a button to apply the changes
+    let apply_button = document.createElement('input');
+    apply_button.type = 'button';
+    apply_button.value = 'Apply Selection';
+    apply_button.onclick = applyChanges;
+    apply_button.classList.add('apply-button');
+
+    // Append the apply button to the form
+    form.append(apply_button);
+
+    // Append the form to the formContainer div
+    formContainer.appendChild(form);
+}
+
+// Initialize the page by loading a form with a button for each SVG available.
+// This is done by reading the paths in the config.json file.
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('./config.json').then(response => response.json()).then(json =>{
+        load_form(json);
+    });
+});
+
+// Updates the displayed SVG by combining the selected SVGs.
+async function applyChanges() {
+    var checkboxes = document.getElementsByName('checkbox');
+    var selectedValues = [];
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            selectedValues.push(checkboxes[i].value);
         }
     }
-    return result;
+    
+    paths = selectedValues.reverse();
+    SVGResult.innerHTML = await SVGContentMerge(paths);
 }
 
-// Creates an SVG from a list of svg objects by joining and appending svg start and end tags.
-function createSVG(contents){
-    let result = []
-    result.push('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-300 -300 600 600">\n');
-    contents.forEach(e => {
-        result.push(e);
-        result.push('\n');
-    });
-    result.push('</svg>');
-    return result.join('');
+
+// For navigation on the svg:
+
+//--- Adjustable paramters:
+// Speed of zoom on scroll.
+const zoomFactor = 1.1;
+// To calculate the maximum width and height of the viewbox.
+const maxHalfLengths = { x: 3000, y: 3000 };
+//----
+
+let pivot = { x: 0, y: 0 };
+let position = { x: 0, y: 0 };
+let halfSize = { x: 300, y: 300 };
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+setViewBox();
+
+SVGResult.addEventListener("mousemove", onMousemove);
+SVGResult.addEventListener("mousedown", onMousedown);
+SVGResult.addEventListener("wheel", onWheel);
+
+const mouse = {
+    position: { x: 0, y: 0 },
+    isDown: false,
+};
+
+function viewBoxDim(){
+    return {x: position.x - halfSize.x, y: position.y - halfSize.y, w: 2*halfSize.x, h: 2*halfSize.y};
 }
 
-// Given a list of paths to svgs, returns an svg of their combined content.
-async function combineSVGS(paths){
-    let contents = await readFiles(paths);
-    contents = contents.map(removeSVGTag);
-    return createSVG(contents);
+function onMousedown(e) {
+    mouse.position = screenToViewBoxPosition(e.pageX, e.pageY);
+    window.addEventListener("mouseup", onMouseup);
+    mouse.isDown = true;
+}
+
+function setViewBox() {
+    let dim = viewBoxDim();
+    SVGResult.setAttribute("viewBox", `${dim.x} ${dim.y} ${dim.w} ${dim.h}`);
+}
+
+function screenToViewBoxPosition(screenX, screenY){
+    return {
+        x: screenX * 2 * halfSize.x/SVGResult.clientWidth,
+        y: screenY * 2 * halfSize.y/SVGResult.clientHeight
+    }
+}
+
+function onMousemove(e) {
+    if (mouse.isDown) {
+        let pos = screenToViewBoxPosition(e.pageX, e.pageY);
+        let dx = (pos.x - mouse.position.x);
+        let dy = (pos.y - mouse.position.y);
+        position = {x: clamp(position.x - dx, -maxHalfLengths.x, maxHalfLengths.x), y: clamp(position.y - dy, -maxHalfLengths.y, maxHalfLengths.y)};
+        mouse.position = pos;
+        setViewBox();
+    }
+}
+
+function onMouseup(e) {
+    window.removeEventListener("mouseup", onMouseup);
+    mouse.isDown = false;
+}
+
+function onWheel(e) {
+    const scale = e.deltaY > 0 ? zoomFactor : 1/zoomFactor;
+    halfSize = {x: clamp(halfSize.x * scale, 1, maxHalfLengths.x), y: clamp(halfSize.y * scale, 1, maxHalfLengths.y)};
+    setViewBox();
 }
 )";
 
@@ -218,12 +266,18 @@ const std::string css_text = R"(.app{
     font-family: monospace;
 }
 
-.result{
-    box-shadow: 0 4px 17px rgba(0, 0, 0, 0.35);
-    width: 60%;
-    height: 60%;
+.result-div{
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.35);
+    height: 40%;
+    width: 40%;
     display: inline-block;
+    cursor: grab;
 }
+
+.result-div svg g{
+    cursor: pointer;
+}
+
 )";
 
 } //  namespace actsvg::web
