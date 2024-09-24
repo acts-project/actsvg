2ac6ef3fd46134729886d4de4e810e158339687f
Andreas Salzburger <asalzburger@gmail.com>
2024-09-24 09:52:50 +0200

    fix eta lines on both sides of the detector

--- tests/meta/eta_lines.cpp
+++ tests/meta/eta_lines.cpp
@@ -17,7 +17,7 @@
 
 using namespace actsvg;
 
-TEST(meta, eta_lines) {
+TEST(meta, eta_lines_positive_side) {
 
     auto pg = test::playground({-300, -10}, {300, 120});
 
@@ -45,7 +45,40 @@ TEST(meta, eta_lines) {
 
     etafile.add_object(eta_lines);
     std::ofstream tstream;
-    tstream.open("test_display_eta_lines.svg");
+    tstream.open("test_display_eta_lines_ps.svg");
+    tstream << etafile;
+    tstream.close();
+}
+
+TEST(meta, eta_lines_full_detector) {
+
+    auto pg = test::playground({-300, -10}, {300, 120});
+
+    svg::file etafile;
+    etafile.add_object(pg);
+
+    scalar r = 120;
+    scalar z = 300;
+
+    std::vector<scalar> eta_main = { -3, -2, -1, 0, 1, 2, 3};
+    style::stroke stroke_main;
+    bool label_main = true;
+    style::font font_main;
+
+    std::vector<scalar> eta_half = {0.5, 1.5, 2.5};
+    style::stroke stroke_half;
+    stroke_half._dasharray = {2, 2};
+    bool label_half = false;
+    style::font font_half;
+
+    auto eta_lines = display::eta_lines(
+        "eta_lines_", z, r,
+        {std::tie(eta_main, stroke_main, label_main, font_main),
+         std::tie(eta_half, stroke_half, label_half, font_half)});
+
+    etafile.add_object(eta_lines);
+    std::ofstream tstream;
+    tstream.open("test_display_eta_lines_fd.svg");
     tstream << etafile;
     tstream.close();
 }
