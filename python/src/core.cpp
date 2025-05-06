e58aebfe4f6660cae4467bf7eda821f74cf754c1
Andreas Salzburger <asalzburger@gmail.com>
2025-05-06 17:42:01 +0200

    remove double declaration

--- python/src/core.cpp
+++ python/src/core.cpp
@@ -44,25 +44,6 @@ void add_core_module(context& ctx) {
             .def("add_object", &svg::object::add_object);
     }
 
-    {
-
-        py::class_<actsvg::svg::file>(c, "file")
-            .def(py::init<>())
-            .def("add_object", &actsvg::svg::file::add_object)
-            .def("add_objects", &actsvg::svg::file::add_objects)
-            .def(
-                "clip",
-                [](actsvg::svg::file& self, std::array<actsvg::scalar, 4> box) {
-                    self.set_view_box(box);
-                })
-            .def("write",
-                 [](actsvg::svg::file& self, const std::string& filename) {
-                     std::ofstream file(filename);
-                     file << self;
-                     file.close();
-                 });
-    }
-
     auto d = m.def_submodule("draw");
 
     {
